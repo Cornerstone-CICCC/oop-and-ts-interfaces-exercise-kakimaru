a3622230ec99452c66ea1b9622b8ebab31a84a24
kakimaru <memimei0306@gmail.com>
2024-10-08 08:26:04 +0900

    I tried exercises

--- src/ex05.ts
+++ src/ex05.ts
@@ -3,15 +3,16 @@
 // Create an object with unknown type, then cast it to the "User" interface and log the email if it exists.
 
 interface User {
-
+  id: number;
+  email?: string;
 }
 
-const unknownUser = {
+const unknownUser: unknown = {
   id: 1,
   email: "test@example.com"
 };
 
-const user = unknownUser;
+const user = unknownUser as User;
 
 console.log(user.email ?? "Email not available");
 // Expected Output: "test@example.com"
